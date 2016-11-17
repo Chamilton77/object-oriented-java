c3cbf6075d44870b692a9b9015db42157483e053
Carson Hamilton <cjcham77@gmail.com>
2016-11-16 23:05:27 -0700

    object oriented javascript assignment

--- OOmini.js
+++ OOmini.js
@@ -1,6 +1,3 @@
-/*
-the way to do the work is to break everything down to the loswest step you can.
-chop everything up to steps and doing the first step to do one of the steps*/
 //This object takes a given number and multiplies the first given number by 1
 //and then mulitplies the value of that number by the given number.
 function Multiplier(num){
@@ -16,21 +13,82 @@ function Multiplier(num){
 }
 
 
-//2
-
+//////////////////////////////////////////////////////////2
+//Set up an album function that holds photo objects in an array 
+//
 function Album(){
-	var photos["photo1, Utah", "photo2, NewYork"];
-	function Photos(filename, location){
-		this.filename = filename;
-		this.location = location;
-		this.photostring = function(){
-			return this.filename, + this.location;
-		}
+	this.photos = [];
+	this.add = function(image){
+		this.photos.push(image);//this adds photos to the array
 	}
-	var newphoto = new Photo ("photo3, California");
-	photos.push(newphoto);
-	
+	this.listPhotos = function(){
+		var list = ""; //this is the function that chooses a photo in the array
+		for(i = 0; i < this.photos.length; i++){
+			list += this.photos[i].file;
+		}
+		return list;
+	}
+	this.specific = function(index){
+		return this.photos[index];//this returns the photo that you got out of the array
+	}
 }
 
-//3
+function Photo(file, location){
+	this.file = file; //this is the constructor for the photo objects that takes parameters
+	this.location = location;
+}
+
+var pic1 = new Photo("Carson", "Utah");
+var pic2 = new Photo("Carson", "Florida");
+
+var album = new Album();
+album.add(pic1);
+album.add(pic2);
+album.listPhotos();
+album.specific(2);
+
+////////////////////////////////////////////////////3
+//set up the Person object and set name 
+var Person = function(name) {
+  this.name = name;
+}
+//set up the student object and set parameters for name and subject
+// get name by calling the Person object
+function Student(name, subject){
+	Person.call(this, name)
+	this.subject = subject;
+	Student.prototype.gotoClass = function(){
+		console.log("I am " + this.name + " and I am going to my " + this.subject + " class!");
+	};
+}
+//set up the teacher object and set parameters
+function Teacher(name, subject){
+	this.subject = subject;
+	Person.call(this,name)
+	Teacher.prototype.sayHello = function(){
+		console.log("Hello my name is " + this.name + " and I teach " + this.subject);
+	};
+}
+//set up the school function with instances of student and teacher
+function School(){
+	var kid1 = new Student ("Tommy", "math");
+	var teach1 = new Teacher ("Mr.Teacher", "English");
+}
+//make new instance of school object
+var school1 = new School();
+//call student and teacher functions
+school1.kid1.gotoClass();
+school1.teach1.sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
 
